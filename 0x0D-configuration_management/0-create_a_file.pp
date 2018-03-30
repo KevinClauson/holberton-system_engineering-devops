f4d71f58f71aa57378cbec22b4cbfd444510e445
KevinClauson <kevinclauson99@gmail.com>
2018-03-30 00:49:31 +0000

    style

--- 0x0D-configuration_management/0-create_a_file.pp
+++ 0x0D-configuration_management/0-create_a_file.pp
@@ -1,9 +1,9 @@
 # create a puppet file
 file {  '/tmp/holberton':
-     ensure	=>	file,
-     path   	=>	'/tmp/holberton',
-     mode   	=>	'0774',
-     owner  	=>	'www-data',
-     group  	=>	'www-data',
-     content	=>	'I love Puppet'
+  ensure  => file,
+  path    => '/tmp/holberton',
+  mode    => '0774',
+  owner   => 'www-data',
+  group   => 'www-data',
+  content => 'I love Puppet'
 }
