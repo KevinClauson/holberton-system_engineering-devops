48f386f66f44210851b06f7ee563a03ca85b9ce0
KevinClauson <kevinclauson99@gmail.com>
2018-05-04 22:56:27 +0000

    add style

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -7,14 +7,14 @@ import requests
 
 
 def number_of_subscribers(subreddit):
-   '''
-   api call with subreddit lookup
-   '''
-   root = 'https://www.reddit.com'
-   path = '/r/{}/about.json'.format(subreddit)
-   url = '{}{}'.format(root, path)
-   header = {'User-Agent': 'Chrome/2602:301:772d:c490:ec20:8e49:3647:8db6'}
-   response = requests.get(url, headers=header, allow_redirects=False)
-   if response.status_code >= 300:
-      return 0
-   return response.json().get('data').get('subscribers')
+    '''
+    api call with subreddit lookup
+    '''
+    root = 'https://www.reddit.com'
+    path = '/r/{}/about.json'.format(subreddit)
+    url = '{}{}'.format(root, path)
+    header = {'User-Agent': 'Chrome/2602:301:772d:c490:ec20:8e49:3647:8db6'}
+    response = requests.get(url, headers=header, allow_redirects=False)
+    if response.status_code >= 300:
+        return 0
+    return response.json().get('data').get('subscribers')
